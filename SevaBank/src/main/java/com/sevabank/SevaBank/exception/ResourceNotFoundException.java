cf2abdf7445408a2c2ba39bdacd74c9fcf72d261
Vishal <vishalgautam.haridwar@gmail.com>
2026-08-25 15:07:37 +0530

    changes in exceptions

--- SevaBank/src/main/java/com/sevabank/SevaBank/exception/ResourceNotFoundException.java
+++ SevaBank/src/main/java/com/sevabank/SevaBank/exception/ResourceNotFoundException.java
@@ -1,6 +1,6 @@
 package com.sevabank.SevaBank.exception;
 
-public class ResourceNotFoundException extends RuntimeException{
+public class ResourceNotFoundException extends CustomServiceException{
 
     public ResourceNotFoundException(String message) {
         super(message);
