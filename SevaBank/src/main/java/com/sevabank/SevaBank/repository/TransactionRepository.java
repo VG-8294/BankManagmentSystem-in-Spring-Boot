c7b822b5861c4f90c6e2634c73bd6eaebf3ea2a6
Vishal <vishalgautam.haridwar@gmail.com>
2026-08-27 14:29:25 +0530

    added exception handling in transaction repo

--- SevaBank/src/main/java/com/sevabank/SevaBank/repository/TransactionRepository.java
+++ SevaBank/src/main/java/com/sevabank/SevaBank/repository/TransactionRepository.java
@@ -1,6 +1,7 @@
 package com.sevabank.SevaBank.repository;
 
 import com.sevabank.SevaBank.entity.Transaction;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -15,17 +16,22 @@ public class TransactionRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public void save(Transaction tx) {
-        String sql = "INSERT INTO transaction_schema.transactions" +
+    public void save(Transaction tx) throws Exception {
+        try{
+            String sql = "INSERT INTO transaction_schema.transactions" +
                     "(amount, balance_after_transaction, transaction_time, transaction_type, account_number) " +
                     "VALUES (?, ?, ?, ?, ?)";
 
-        jdbcTemplate.update(sql,
-                     tx.getAmount(),
-                     tx.getBalanceAfterTransaction(),
-                     LocalDateTime.now(),
-                     tx.getTransactionType().name(),
-                     tx.getBankAccount().getAccNo()
-                );
+            jdbcTemplate.update(sql,
+                    tx.getAmount(),
+                    tx.getBalanceAfterTransaction(),
+                    LocalDateTime.now(),
+                    tx.getTransactionType().name(),
+                    tx.getBankAccount().getAccNo()
+            );
+        }
+        catch (DataAccessException e){
+            throw new Exception("Some error in creating transaction");
+        }
     }
 }
